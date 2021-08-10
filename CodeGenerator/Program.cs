c6f77686c5ff8f0264c69fd6e3a6a7242aae2922
Marcelo Cavalini <38933130+mpcavalini@users.noreply.github.com>
2021-08-11 00:05:55 +1000

    Code generator

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -8,6 +8,9 @@ namespace CodeGenerator
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Project prefix:");
+            var cprojectPrefix = "MyGrade";// Console.ReadLine();
+
             Console.WriteLine("Class name:");
             var cclass = Console.ReadLine();
 
@@ -16,31 +19,39 @@ namespace CodeGenerator
 
             Console.WriteLine($"Class name:{cclass}");
             Console.WriteLine($"Project name:{cproject}");
+            Console.WriteLine($"Project prefix:{cprojectPrefix}");
 
-            var app = new App(cclass, cproject);
-            app.Init();
-            Console.ReadLine();
+            new App(cclass, cproject, cprojectPrefix);
+
+            //Console.ReadLine();
         }
     }
 
     public class App
     {
         public string projectTarget { get; set; }
+        public string projectPrefix { get; set; }
         public string myClass { get; set; }
         public string myProject { get; set; }
         public string myNamespace { get; set; }
 
-        public App(string cclass, string cproject)
+        public App(string cclass, string cproject, string cprojectPrefix)
         {
             //projectTarget = @"Z:\dev\netcore\MyGrade\MyGrade\__CC";
-            projectTarget = @"Z:\dev\netcore\MyGrade\MyGrade";
+            projectTarget = @"C:\Users\Cavas\Documents\sites\MyGrade\MyGrade";
+            projectPrefix = cprojectPrefix;
             myClass = cclass;
             myProject = cproject;
+
+            Init();
         }
 
         public void Init() {
             Console.WriteLine("Init");
             Domain();
+            RepositoryInterface();
+            EntityConfiguration();
+            Repository();
             Console.WriteLine("End");
         }
 
@@ -54,7 +65,7 @@ namespace CodeGenerator
             if (!File.Exists(path))
             {
 
-                myNamespace = setNameSpace("MyGrade.Core.Domain");
+                myNamespace = setNameSpace("Core.Domain");
 
                 // Create a file to write to.
                 using (StreamWriter sw = File.CreateText(path))
@@ -69,6 +80,101 @@ namespace CodeGenerator
                 }
             }
             Console.WriteLine($"{myClass} Domain created");
+        }
+
+        private void RepositoryInterface()
+        {
+
+            string src = @"\Core\Repositories";
+
+            string path = CreateFolder(src);
+            path = @$"{path}\I{myClass}Repository.cs";
+            if (!File.Exists(path))
+            {
+
+                myNamespace = setNameSpace("Core.Repositories");
+
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"using {setNameSpace("Core.Domain")};");
+                    sw.WriteLine($"namespace {myNamespace}");
+                    sw.WriteLine("\t{");
+                    sw.WriteLine($"\t\tpublic interface I{myClass}Repository : IRepository<{myClass}>");
+                    sw.WriteLine("\t\t{");
+                    sw.WriteLine("\t\t}");
+                    sw.WriteLine("\t}");
+                }
+            }
+            Console.WriteLine($"{myClass} RepositoryInterface created");
+        }
+
+        private void EntityConfiguration()
+        {
+
+            string src = @"\Persistence\EntityConfigurations";
+
+            string path = CreateFolder(src);
+            path = @$"{path}\{myClass}Configuration.cs";
+            if (!File.Exists(path))
+            {
+
+                myNamespace = setNameSpace("Persistence.EntityConfigurations");
+
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"using {setNameSpace("Core.Domain")};");
+                    sw.WriteLine($"using System.Data.Entity.ModelConfiguration;");
+                    sw.WriteLine($"namespace {myNamespace}");
+                    sw.WriteLine("\t{");
+                    sw.WriteLine($"\t\tpublic class {myClass}Configuration : EntityTypeConfiguration<{myClass}>");
+                    sw.WriteLine("\t\t{");
+                    sw.WriteLine($"\t\t\tpublic {myClass}Configuration()");
+                    sw.WriteLine("\t\t\t{");
+                    sw.WriteLine("\t\t\t}");
+                    sw.WriteLine("\t\t}");
+                    sw.WriteLine("\t}");
+                }
+            }
+            Console.WriteLine($"{myClass} EntityConfiguration created");
+        }
+
+        private void Repository()
+        {
+
+            string src = @"\Persistence\Repositories";
+
+            string path = CreateFolder(src);
+            path = @$"{path}\{myClass}Repository.cs";
+            if (!File.Exists(path))
+            {
+
+                myNamespace = setNameSpace("Persistence.Repositories");
+
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine($"using {setNameSpace("Core.Domain")};");
+                    sw.WriteLine($"using {setNameSpace("Core.Repositories")};");
+                    sw.WriteLine($"using System.Data.Entity;");
+                    sw.WriteLine($"using System.Linq;");
+                    sw.WriteLine($"namespace {myNamespace}");
+                    sw.WriteLine("\t{");
+                    sw.WriteLine($"\t\tpublic class {myClass}Repository : Repository<{myClass}>, I{myClass}Repository");
+                    sw.WriteLine("\t\t{");
+                    sw.WriteLine($"\t\t\tpublic {myClass}Repository(PlutoContext context) : base(context)");
+                    sw.WriteLine("\t\t\t{");
+                    sw.WriteLine("\t\t\t}");
+                    sw.WriteLine($"\t\t\tpublic PlutoContext PlutoContext");
+                    sw.WriteLine("\t\t\t{");
+                    sw.WriteLine("\t\t\t\tget { return Context as PlutoContext; }");
+                    sw.WriteLine("\t\t\t}");
+                    sw.WriteLine("\t\t}");
+                    sw.WriteLine("\t}");
+                }
+            }
+            Console.WriteLine($"{myClass} Repository created");
         }
 
         private string CreateFolder(string src)
@@ -87,13 +193,15 @@ namespace CodeGenerator
 
         private string setNameSpace(string val)
         {
-            return String.IsNullOrEmpty(myProject) ? val : $"{val}.{myProject}";
+
+            return String.IsNullOrEmpty(myProject) ? $"{projectPrefix}.{val}" : $"{projectPrefix}.{val}.{myProject}";
         }
 
         private string setFolder(string val)
         {
             return String.IsNullOrEmpty(myProject) ? val : @$"{val}\{myProject}";
         }
+
     }
 
 }
